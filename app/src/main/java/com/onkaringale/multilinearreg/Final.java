2b823b131ba95061c17102d70e144aea78444bb9
onkaringale <ingale2273@gmail.com>
2022-03-27 22:43:56 +0530

    5.0 Éclair

--- app/src/main/java/com/onkaringale/multilinearreg/Final.java
+++ app/src/main/java/com/onkaringale/multilinearreg/Final.java
@@ -1,10 +1,17 @@
 package com.onkaringale.multilinearreg;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,9 +20,102 @@ import android.widget.Toast;
 import com.example.cpp.R;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Final extends AppCompatActivity {
+
+    private static final String TAG = "Test";
+    private static String[] stored_values;
+
+    public static class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+        private static String[] mDataset;
+
+        public MyAdapter(String[] myDataset)
+        {
+            mDataset = myDataset;
+            stored_values = new String[myDataset.length];
+        }
+
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapt_edittext, parent, false);
+            ViewHolder vh = new ViewHolder(v, new ViewHolder.ITextWatcher() {
+                @Override
+                public void beforeTextChanged(int position, CharSequence s, int start, int count, int after) {
+                    // do something
+                }
+
+                @Override
+                public void onTextChanged(int position, CharSequence s, int start, int before, int count) {
+                    stored_values[position] = s.toString();
+                }
+
+                @Override
+                public void afterTextChanged(int position, Editable s) {
+                    // do something
+                }
+            });
+
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, final int position) {
+//            holder.mEditText.setHint(mDataset[position]);
+            holder.mText.setText(mDataset[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
+
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+
+            public EditText mEditText;
+            public TextView mText;
+            private ITextWatcher mTextWatcher;
+
+            public interface ITextWatcher {
+                // you can add/remove methods as you please, maybe you dont need this much
+                void beforeTextChanged(int position, CharSequence s, int start, int count, int after);
+
+                void onTextChanged(int position, CharSequence s, int start, int before, int count);
+
+                void afterTextChanged(int position, Editable s);
+            }
+
+            public ViewHolder(View v, ITextWatcher textWatcher) {
+                super(v);
+
+                this.mEditText = (EditText) v.findViewById(R.id.etadap_editText);
+                this.mText = (TextView) v.findViewById(R.id.etadap_Text);
+                this.mTextWatcher = textWatcher;
+
+                this.mEditText.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                        mTextWatcher.beforeTextChanged(getAdapterPosition(), s, start, count, after);
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        mTextWatcher.onTextChanged(getAdapterPosition(), s, start, before, count);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        mTextWatcher.afterTextChanged(getAdapterPosition(), s);
+                    }
+                });
+            }
+        }
+
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,37 +125,66 @@ public class Final extends AppCompatActivity {
 
         Bundle bundle = intent.getExtras();
 
+        /*
+            []output = [r2score,betas...]
+            []header = [all headers]
+            []header_order = [needed headers only with index] use: for 0 to ho.size() print(header[ho.get(i)])
+
+         */
+
         String []output = bundle.getStringArray("result");
         String []header = bundle.getStringArray("header");
         ArrayList<Integer> header_order = (ArrayList<Integer>) bundle.getSerializable("header_order");
 
         TextView r2score = findViewById(R.id.r2score);
-        r2score.setText(("R2 Score\n"+output[0]));
-        EditText editText = findViewById(R.id.predict_x);
+        r2score.setText(("R-Squared Score\n"+output[0]));
+//        EditText editText = findViewById(R.id.predict_x);
         TextView answer = findViewById(R.id.predict_ans);
         Button predict = findViewById(R.id.predictbtn);
-        StringBuilder eheader = new StringBuilder();
-        for (int i = 0; i<header_order.size() -1;i++)
-        {
-            if (i == header_order.size()-2)
-                eheader.append(header[header_order.get(i)]);
-            eheader.append(header[header_order.get(i)]).append(", ");
-        }
+//        StringBuilder eheader = new StringBuilder();
+//        for (int i = 0; i<header_order.size() -1;i++)
+//        {
+//            if (i == header_order.size()-2)
+//                eheader.append(header[header_order.get(i)]);
+//            eheader.append(header[header_order.get(i)]).append(", ");
+//        }
 //        editText.setText(eheader);
+
+        String[] oheader = new String[header_order.size()-1];
+        for (int i = 0; i<oheader.length;i++)
+        {
+                oheader[i] = header[header_order.get(i)];
+        }
+
+        RecyclerView editView = findViewById(R.id.edrv);
+        editView.setLayoutManager(new LinearLayoutManager(this));
+        editView.setAdapter(new MyAdapter(oheader));
+
+
+
+
+        Log.v(TAG, "onCreate: " );
         predict.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                ArrayList<String> x = new ArrayList<>(Arrays.asList(editText.getText().toString().split(",")));
-                String[] x = editText.getText().toString().split("\\s*,\\s*");
-                if (x.length != output.length-2)
-                {
-                    Toast.makeText(getApplicationContext(),"Error: Wrong Input",Toast.LENGTH_SHORT).show();
-                    return;
+//                String[] x = editText.getText().toString().split("\\s*,\\s*");
+//                if (x.length != output.length-2)
+//                {
+//                    Toast.makeText(getApplicationContext(),"Error: Wrong Input",Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+                for (int i = 0; i < stored_values.length; i++) {
+                    if (stored_values[i] == ("") ||stored_values[i]==null||stored_values[i].isEmpty())
+                    {
+                        Toast.makeText(getApplicationContext(),"Please fill the "+ header[header_order.get(i)]+" field.",Toast.LENGTH_SHORT).show();
+                        return;
+                    }
                 }
                 double predict_output = Double.parseDouble(output[1]);
                 for (int i = 2; i < output.length; i++)
                 {
-                    predict_output += (Double.parseDouble(output[i]) * Double.parseDouble(x[i-2]));
+                    predict_output += (Double.parseDouble(output[i]) * Double.parseDouble(stored_values[i-2]));
                 }
                 answer.setText((header[header_order.get(header_order.size() - 1)] + " : "+predict_output));
 
--- app/src/main/java/com/onkaringale/multilinearreg/MainActivity.java
+++ app/src/main/java/com/onkaringale/multilinearreg/MainActivity.java
@@ -16,18 +16,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cpp.R;
 import com.example.cpp.databinding.ActivityMainBinding;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.ui.PlayerView;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
+
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     VideoView videoView;
+    ExoPlayer exoplayer;
 
     public static final int myrequestCode = 1014;
     private ActivityMainBinding binding;
@@ -40,21 +47,35 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        videoView = (VideoView) findViewById(R.id.videoview);
-        String vpath = "android.resource://" + getPackageName() + "/" + R.raw.intro;
-        videoView.setVideoURI(Uri.parse(vpath));
+        String vpath = "android.resource://" + getPackageName() + "/" + R.raw.reintroweb;
+
+        exoplayer = new ExoPlayer.Builder(this).build();
+        PlayerView playerView = findViewById(R.id.videoview);
+        playerView.setPlayer(exoplayer);
+        playerView.setUseController(false);
+        MediaItem mediaItem= MediaItem.fromUri(Uri.parse(vpath));
+        exoplayer.addMediaItem(mediaItem);
+        exoplayer.setRepeatMode(exoplayer.REPEAT_MODE_ONE);
+        exoplayer.prepare();
+        exoplayer.play();
+
+
+
+//        videoView = (VideoView) findViewById(R.id.videoview);
+
+//        videoView.setVideoURI(Uri.parse(vpath));
 //        MediaController mediaController;
 //        mediaController = new MediaController(MainActivity.this);
 //        mediaController.setAnchorView(videoView);
 //        videoView.setMediaController(mediaController);
-        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                mp.setVolume(0f,0f);
-                mp.setLooping(true);
-            }
-        });
-        videoView.start();
+//        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+//            @Override
+//            public void onPrepared(MediaPlayer mp) {
+//                mp.setVolume(0f,0f);
+//                mp.setLooping(true);
+//            }
+//        });
+//        videoView.start();
 
         Button fcbtn = (Button) findViewById(R.id.choosefilebtn);
         fcbtn.setOnClickListener(new View.OnClickListener() {
@@ -75,9 +96,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        if(videoView != null)
+        if(exoplayer != null)
         {
-            videoView.start();
+            exoplayer.play();
         }
     }
 
@@ -96,11 +117,35 @@ public class MainActivity extends AppCompatActivity {
                         InputStream input = this.getContentResolver().openInputStream(uri);
                         CSVReader reader = new CSVReader(new InputStreamReader(input));
                         dataset = reader.readAll();
+                        ArrayList<Integer> drop_indexes = new ArrayList<>();
+
+                        //Drop Implementation
+                        for (int i = 0; i < dataset.get(1).length; i++)
+                        {
+                            try {
+                                Double.parseDouble(dataset.get(1)[i]);
+                            }
+                            catch (NumberFormatException e )
+                            {
+                                drop_indexes.add(i);
+                            }
+                        }
+
+                        if (0<drop_indexes.size())
+                        {
+                            for (int i = 0; i < dataset.size(); i++)
+                            {
+                                dataset.set(i,drop_index(dataset.get(i),drop_indexes));
+                            }
+                        }
+                        //Drop Implementation End
+
                         Intent intent_csvdashb = new Intent(MainActivity.this,csv_dashboard.class);
                         intent_csvdashb.putExtra("size",dataset.size());
                         for (int i = 0; i < dataset.size(); i++) {
                             intent_csvdashb.putExtra( ("row_"+i),dataset.get(i));
                         }
+//                        exoplayer.release();
                         startActivity(intent_csvdashb);
                     } catch (IOException | CsvException e) {
                         e.printStackTrace();
@@ -109,6 +154,22 @@ public class MainActivity extends AppCompatActivity {
                 }
 
         }
+    }
+
+    public String[] drop_index(String[] a,ArrayList<Integer> index)
+    {
+        ArrayList<String> a_al = new ArrayList<String>(Arrays.asList(a));
+        for (int i = 0; i < index.size(); i++)
+        {
+            int temp = index.get(i);
+            a_al.remove(temp);
+        }
+        String[] b = new String[a_al.size()];
+        for (int i = 0; i < b.length; i++)
+        {
+            b[i] = a_al.get(i);
+        }
+        return b;
     }
 
 
